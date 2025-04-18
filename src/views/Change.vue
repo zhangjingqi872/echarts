14549df8f3973e07d28e2c54cd1116cb81ad1290
Cythnia Nong <69142865+zhangjingqi872@users.noreply.github.com>
2025-04-18 09:35:00 +0800

    Create Change.vue

---
+++ src/views/Change.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Change />
+  </div>
+</template>
+
+<script>
+import Change from '../components/Change.vue'
+
+export default {
+  name: 'ChangeView',
+  components: {
+    Change
+  }
+}
+</script>
+
+<style scoped>
+/* 样式已通过全局CSS导入 */
+</style>
